bc285378e2a6c3d19ed0fcad99d3f8e088a91533
jeampierk <jeanpierks6@gmail.com>
2023-09-08 11:05:12 -0500

    Se corrigio un error de enlazado en la cabezera

--- Frontend/scripts/form.js
+++ Frontend/scripts/form.js
@@ -1,4 +1,6 @@
 const header_ht=document.querySelector('header');
 import {header} from './head.js';
-const respuesta=header('./img/fk.png');
+const respuesta=header('../img/fk.png');
 header_ht.innerHTML=respuesta;
+ 
+
--- Frontend/scripts/head.js
+++ Frontend/scripts/head.js
@@ -1,7 +1,10 @@
-export const header_=` <div class="box-header">
-<a href=""><img src="./img/fk.png" alt="logo-empresa"></a>
-<a href="">Algo</a>
-<a href="">Preguntas Frecuentes</a>
-<a href=""> Blog</a>
-<a href="" class="button-contact">Contacto</a>
-</div>`;
+export function header(ruta){
+    
+    return ` <div class="box-header">
+    <a href=""><img src="${ruta}" alt="logo-empresa"></a>
+    <a href="">Algo</a>
+    <a href="">Preguntas Frecuentes</a>
+    <a href=""> Blog</a>
+    <a href="" class="button-contact">Contacto</a>
+    </div>`;
+}
