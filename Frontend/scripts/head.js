d2dda9a48e1960c57a9bab67a30ff59edd8508b3
jeampierk <jeanpierks6@gmail.com>
2023-09-08 09:48:09 -0500

    reorganizando los archivos

---
+++ Frontend/scripts/head.js
@@ -0,0 +1,7 @@
+export const header_=` <div class="box-header">
+<a href=""><img src="./img/fk.png" alt="logo-empresa"></a>
+<a href="">Algo</a>
+<a href="">Preguntas Frecuentes</a>
+<a href=""> Blog</a>
+<a href="" class="button-contact">Contacto</a>
+</div>`;
---
+++ Frontend/scripts/main.js
@@ -0,0 +1,5 @@
+const header=document.querySelector('header');
+
+import { header_ } from "./head.js";
+
+header.innerHTML=header_;
