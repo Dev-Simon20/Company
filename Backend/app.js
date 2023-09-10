0a234142a37c613cc1c67a78377fe3f3972ffdb7
jeampierk <jeanpierks6@gmail.com>
2023-09-09 21:03:46 -0500

    Se implemento el envio de emails

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,50 @@
 const express= require('express');
+const cors=require('cors');
 const app=express();
+const nodemailer=require('nodemailer');
+app.use(express.json());
+app.use(cors());
+
+enviarEmail= async(asunto,texto)=>{
+   const config={
+    host:'smtp.gmail.com',
+    port:587,
+    auth:{
+        user:'jeanpierks6@gmail.com',
+        pass: 'mgsmrhnrcrtlbeqw'
+    }
+   }
+   const mensaje={
+    from:'jeanpierks@gmail.con',
+    to:'jeanpierks6@gmail.com',
+    subject:asunto,
+    text:texto
+   }
+
+   const transport=nodemailer.createTransport(config);
+   const info= await transport.sendMail(mensaje);
+   console.log('mensaje eniviado con exito');
+}
+
+
+app.get('/',(req,res)=>{
+   res.send("hola que tal");
+   console.log("respondiendo en el servidor");
+})
+
+app.post('/',(req,res)=>{
+    const dato=req.body;
+    const asunto=dato.asunto;
+    const texto=`${dato.nombres}\n${dato.correo}\n${dato.telefono}\n${dato.mensaje}`;
+    enviarEmail(asunto,texto);
+})
+
+
+
+
 
 
 
 app.listen(3000,()=>{
-    console.log('Servidor corriendo ene le peurto 3000');
+    console.log('Servidor corriendo ene le puertoo 3000');
 });
--- Frontend/scripts/form.js
+++ Frontend/scripts/form.js
@@ -7,17 +7,31 @@ header_ht.innerHTML = respuesta;
 
 form.addEventListener("submit",(event) => {
     event.preventDefault();
-    const nombre = document.querySelector("#nombre");
-    const empresa = document.querySelector("#empresa");
+    const nombres = document.querySelector("#nombre");
+    const asunto = document.querySelector("#empresa");
     const correo = document.querySelector("#correo");
     const telf = document.querySelector("#num");
     const mensaje = document.querySelector("#mens");
-    console.log(nombre.value,empresa.value,correo.value,telf.value,mensaje.value);
-    nombre.value='';
-    empresa.value='';
+    let corpu={
+      asunto:asunto.value,
+      nombres:nombres.value,
+      correo:correo.value,
+      telefono:telf.value,
+      mensaje:mensaje.value
+    }
+    let corpuJson=JSON.stringify(corpu);
+    console.log(corpuJson);
+    fetch("http://localhost:3000/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: corpuJson
+    });
+
+    nombres.value='';
+    asunto.value='';
     correo.value='';
     telf.value='';
     mensaje.value='';
   });
-
-
